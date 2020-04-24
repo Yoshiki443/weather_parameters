0fbb4e74c9bd26e5ed1aeecba142f9feea552d61
Yoshiki Kato <calmtree443@YoshikinoMacBook-Pro.local>
2020-04-24 19:14:06 +0900

    Change long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="wxparams",
     version="1.0",
     author="Yoshiki Kato",
     # author_email="",
     description="Weather Parameters Calculator",
-    long_description=long_description,
+    long_description="This is a python module for calculating meteorological parameters.",
     long_description_content_type="text/markdown",
     url="https://github.com/Yoshiki443/weather_parameters",
     packages=setuptools.find_packages(),
