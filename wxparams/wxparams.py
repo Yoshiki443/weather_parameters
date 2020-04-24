bf257e6c17ca34935c1ff3f1bfaa26905116176d
Yoshiki Kato <calmtree443@YoshikinoMacBook-Pro.local>
2020-04-24 18:30:59 +0900

    Change descriptions in the script from Japanese to English

--- wxparams/wxparams.py
+++ wxparams/wxparams.py
@@ -3,9 +3,9 @@
  Weather Parameters | Python module
 ======================================
 
-気象に関する関数を集めたpythonモジュールです。
-- 気象要素の計算
-- 単位変換
+Python module for calculating meteorological parameters
+- Meteorological parameters
+- Unit conversion
 
 Copyright (c) Yoshiki Kato @ Weather Data Science
 
@@ -16,13 +16,12 @@ import copy
 
 
 """
-*----------*
- 風を扱う関数
-*----------*
+*----------------------*
+ Wind-related functions
+*----------------------*
 
-風のUV成分[m/s, kt, ...etc]から風向風速を計算する
-真北は0ではなく、360とする
-風速が0だった場合、風向も0とする
+Calculate wind velocity[m/s, kt, etc] and wind direction[degree]
+given U and V components of winds[m/s, kt, etc].
 """
 def UV_to_SpdDir(u,v):
     wspd = np.sqrt( u**2 + v**2 )
@@ -33,7 +32,8 @@ def UV_to_SpdDir(u,v):
     return wspd, wdir
 
 """
-風向[degree]風速[m/s, kt, ...etc]からUV成分を計算する
+Calculate U and V components of winds[m/s, kt, etc]
+given wind velocity[m/s, kt, etc] and wind direction[degree].
 """
 def SpdDir_to_UV(wspd, wdir):
 	u = - wspd * np.sin( np.deg2rad(wdir) )
@@ -41,9 +41,8 @@ def SpdDir_to_UV(wspd, wdir):
 	return u, v
 
 """
-風向を360度から8方位に変換する
-戻り値はN, NE, E...など方位を表すアルファベットとなる
-風向0の場合は指定した文字列に変換される（デフォルトはNone）
+Convert 360-degree wind direction into 8 directions of winds
+like "N, NE, E, SE, S, SW, W, NW", or "8, 1, 2, 3, 4, 5, 6, 7".
 """
 def Deg_to_Dir8(val, dir_zero=None, numeric=False):
     dirname = ['N','NE','E','SE','S','SW','W','NW', dir_zero]
@@ -63,9 +62,9 @@ def Deg_to_Dir8(val, dir_zero=None, numeric=False):
         return np.frompyfunc(lambda x: dirname[x], 1, 1)(deg)
 
 """
-風向を360度から16方位に変換する
-戻り値はN, NNE, NE...など方位を表すアルファベットとなる
-風向0の場合は指定した文字列に変換される（デフォルトはNone）
+Convert 360-degree wind direction into 16 directions of winds,
+like "N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW",
+or "16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15".
 """
 def Deg_to_Dir16(val, dir_zero=None, numeric=False):
     dirname = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW',dir_zero]
@@ -85,9 +84,9 @@ def Deg_to_Dir16(val, dir_zero=None, numeric=False):
         return np.frompyfunc(lambda x: dirname[x], 1, 1)(deg)
 
 """
-空港の滑走路における横風成分(Cross Wind), 追い風成分(Tail Wind), 向かい風成分(Head Wind)を計算
-インプットは風速[m/s, kt, ...etc]、風向[degree]
-滑走路は2桁のRWY番号ではなく、360度でインプットする
+Calculate cross wind, tail wind, and head wind component of wind
+given wind velocity[m/s, kt, etc], wind direction[degree],
+and runway direction[degree] of a airpot.
 """
 def Cross_Wind(wspd, wdir, rwy):
     return np.abs( wspd * np.sin( np.deg2rad(wdir - rwy) ) )
@@ -100,14 +99,12 @@ def Head_Wind(wspd, wdir, rwy):
 
 
 """
-*-------------------*
- 大気中の水分量を扱う関数
-*-------------------*
+*--------------------------*
+ Moisture-related functions
+*--------------------------*
 
-気温[C]と相対湿度[%]から、露点温度[C]を計算する
-飽和水蒸気圧[hPa]の計算は、デフォルトBoltonの式を使用
-オプションでTetensの式、WMO近似式も指定可能
-相対湿度0%だと計算ができないので、0.1%を最小値としている
+Calculate dew point temperature[C]
+given air temperature[C] and relative humidity[%].
 """
 def RH_to_Td(t, rh, formula="Bolton"):
     rh = rh.clip(0.1, None)
@@ -116,9 +113,8 @@ def RH_to_Td(t, rh, formula="Bolton"):
     return WVP_to_T(e, formula)
 
 """
-気温[C]と露点温度[C]から、相対湿度[%]を計算する
-飽和水蒸気圧の計算は、デフォルトBoltonの式を使用
-オプションでTetensの式、WMO近似式も指定可能
+Calculate relative humidity[%]
+given air temperature[C] and dew point temperature[C].
 """
 def Td_to_RH(t, td, formula="Bolton"):
     es = T_to_WVP(t, formula)
@@ -126,12 +122,12 @@ def Td_to_RH(t, td, formula="Bolton"):
     return 100 * e / es
 
 """
-気温[C]から飽和水蒸気圧[hPa]を計算する
-露点温度[C]を入力すれば水蒸気圧[hPa]が計算される
-デフォルトはBoltonの式を使う
-(上) Tetensの式
-(中) WMO近似式
-(下) Boltonの式
+Calculate saturated water vapor pressure[hPa] given air temperature[C].
+If dew point temperature[C] is input, output water vapor pressure[hPa].
+There are 3 formulas to calculate this.
+(1st) Tetens equation
+(2nd) WMO approximation equation
+(3rd) Bolton equation
 """
 def T_to_WVP(t, formula="Bolton"):
     if formula == "Tetens":
@@ -142,12 +138,11 @@ def T_to_WVP(t, formula="Bolton"):
         return 6.112 * np.exp( 17.67 * t / (t + 243.5) )
 
 """
-飽和水蒸気圧[hPa]から気温[C]を計算する
-水蒸気圧[hPa]を入力すれば露点温度[C]が計算される
-デフォルトはBoltonの式を使う
-(上) Tetensの式
-(中) WMO近似式
-(下) Boltonの式
+Calculate air temperature[C] given saturated water vapor pressure[hPa].
+If water vapor pressure[hPa] is input, output dew point temperature[C].
+(1st) Tetens equation
+(2nd) WMO approximation equation
+(3rd) Bolton equation
 """
 def WVP_to_T(es, formula="Bolton"):
     if formula == "Tetens":
@@ -158,13 +153,14 @@ def WVP_to_T(es, formula="Bolton"):
         return 243.5 / ( 17.67/np.log(es/6.112) - 1 )
 
 """
-気温と露点温度から、湿数を計算する
+Calculate dew point depression
+given air temperature[C] and dew point temperature[C].
 """
 def T_Td(t, td):
     return t - td
 
 """
-露点温度[C]と気圧[hPa]から、混合比[g/g]を計算する
+Calculate mixing ratio[g/g] given dew point temperature[C] and pressure[hPa].
 """
 def Mixing_Ratio(td, p, formula="Bolton"):
     e = T_to_WVP(td, formula)
@@ -172,12 +168,11 @@ def Mixing_Ratio(td, p, formula="Bolton"):
 
 
 """
-*----------------*
- 大気安定度に係る関数
-*----------------*
+*-----------------------------*
+ Instability-related functions
+*-----------------------------*
 
-温位[K]を計算する
-インプットする気温は[C]単位、気圧は[hPa]単位
+Calculate potential temperature[K] given air temperature[C] and pressure[hPa].
 """
 def Theta(t, p):
     abs_t = 273.15
@@ -185,17 +180,17 @@ def Theta(t, p):
     return (t + abs_t) * ( (1000./p) ** R_div_Cp )
 
 """
-持ち上げ凝結高度の温度[K]を計算する
-インプットする気温・露点温度とも[K]単位
+Calculate air temperature at lifted condensation level[K]
+given air temperature[K] and dew point temperature[K].
+The unit of air temperature and dew point temperature should be [K].
 """
 def Tlcl(t, td):
     return 1 / ( 1 / (td - 56) + np.log(t/td) / 800 ) + 56
 
 """
-相当温位[K]を計算する
-インプットする気温・露点温度は[C]単位、気圧は[hPa]単位
-計算式は気象庁が採用した算出方法に準ずる
-https://www.data.jma.go.jp/add/suishin/jyouhou/pdf/371.pdf
+Calculate equivalent potential temperature[K]
+given air temperature[C], dew point temperature[C], and pressure[hPa].
+Reference : https://www.data.jma.go.jp/add/suishin/jyouhou/pdf/371.pdf
 """
 def Theta_e(t, td, p, formula="Bolton"):
     abs_t = 273.15
@@ -211,8 +206,11 @@ def Theta_e(t, td, p, formula="Bolton"):
     return t * (1000./(p-e))**R_div_Cp * (t/t_lcl)**(0.28*m) * np.exp( (3036./t_lcl - 1.78) * m * (1 + 0.448 * m ) )
 
 """
-SSIを計算する
-インプットする気温・露点温度は[C]単位、気圧は[hPa]単位
+Calculate showalter stability index (SSI).
+Input parameters are air temperature[C], dew point temperature[C],
+and pressure[hPa] at the base level of an air parcel to be lifted,
+and air temperature[C] and pressure[hPa] at the destination level
+of the lifting air parcel.
 """
 def SSI(p0, p1, t0, t1, td0, formula="Bolton"):
     ept = Theta_e(t0, td0, p0, formula)
@@ -235,21 +233,23 @@ def SSI(p0, p1, t0, t1, td0, formula="Bolton"):
     return t1 - tl
 
 """
-K-Index
-850hPaの気温[C]、露点温度[C]、700hPaの気温[C]、露点温度[C]、500hPaの気温[C]から計算
+Calculate K-Index
+given air temperature[C] and dew point temperature[C] at 850hPa,
+air temperature[C] and dew point temperature[C] at 700hPa,
+and air temperature[C] at 500hPa.
 """
 def K_Index(T850, Td850, T700, Td700, T500):
 	return ( T850 - T500 ) + Td850 - ( T700 - Td700 )
 
 
 """
-*-------*
- 単位変換
-*-------*
+*---------------*
+ Unit conversion
+*---------------*
 
-速度の単位変換
-(上) m/s  -> knot
-(下) knot -> m/s
+Unit of wind speed
+(1st) m/s  -> knot
+(2nd) knot -> m/s
 """
 def MPS_to_KT(x):
     return x / 0.51444
@@ -258,9 +258,9 @@ def KT_to_MPS(x):
     return x * 0.51444
 
 """
-速度の単位変換
-(上) meter -> feet
-(下) feet  -> meter
+Unit of length
+(1st) meter -> feet
+(2nd) feet  -> meter
 """
 def M_to_FT(x):
     return x / 0.3048
